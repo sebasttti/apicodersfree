2cfbb386e5b415cd8b8899162ab97d61312f5e76
sebasttti <juansjoya_r@hotmail.com>
2024-04-23 12:27:54 -0500

    feat: Register users

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -4,17 +4,26 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
     public function store(Request $request){
 
+        $time = Carbon::now()->format("Y-m-d H:i:s");
+
         $request->validate(['name'=>'required|string|min:10',
         'email'=>'required|email|unique:users',
         'password'=>'required|string|min:8|confirmed']);
 
-        $user = User::create($request->all());
+        $userToCreate =  $request->all();
+
+        $userToCreate['created_at'] = $time;
+        $userToCreate['updated_at'] = $time;
+        
+
+        $user = User::create($userToCreate);
 
         return response($user, 201);
 
